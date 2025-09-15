ea489188ac1082c0662d3f29d18d6db00c0fc44c
bhartimishra-dev <bharti.mishra@gobuymybills.com>
2025-09-15 15:49:05 +0530

    test gihub actions

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import { ValidationPipe } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import { ResponseInterceptor } from "./middleware/response.interceptor";
-import { GlobalExceptionFilter } from "./middleware/exception.filter";
-import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ResponseInterceptor } from './middleware/response.interceptor';
+import { GlobalExceptionFilter } from './middleware/exception.filter';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix("api");
-  
+  app.setGlobalPrefix('api');
+
   // Increase payload size limit for file uploads
   const express = require('express');
   app.use(express.json({ limit: '10mb' }));
@@ -22,20 +22,20 @@ async function bootstrap() {
     }),
   );
   app.enableCors({
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
   const config = new DocumentBuilder()
-    .setTitle("Incollab Application Backend")
-    .setDescription("API documentation for the Incollab application")
-    .setVersion("1.0")
+    .setTitle('Incollab Application Backend')
+    .setDescription('API documentation for the Incollab application')
+    .setVersion('1.0')
     .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("api/docs", app, document, {
+  SwaggerModule.setup('api/docs', app, document, {
     swaggerOptions: {
       persistAuthorization: true,
     },
@@ -45,7 +45,7 @@ async function bootstrap() {
   app.useGlobalFilters(new GlobalExceptionFilter());
 
   const configService = app.get(ConfigService);
-  const port = configService.get("PORT") || 3002;
+  const port = configService.get('PORT') || 3002;
   await app.listen(port);
   console.log(`Incollab is running on: http://localhost:${port}/api`);
 }
