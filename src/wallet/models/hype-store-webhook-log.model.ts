a72f7cb5c291013464dd66595e1398ff214b4555
bhartimishra-dev <bharti.mishra@gobuymybills.com>
2026-03-18 13:54:24 +0530

    fix: test webhook transaction aborted issue with the help of logs

--- src/wallet/models/hype-store-webhook-log.model.ts
+++ src/wallet/models/hype-store-webhook-log.model.ts
@@ -28,48 +28,56 @@ export class HypeStoreWebhookLog extends Model<HypeStoreWebhookLog> {
   @Column({
     type: DataType.INTEGER,
     allowNull: true,
+    field: 'hype_store_id',
   })
   declare hypeStoreId: number;
 
   @Column({
     type: DataType.STRING(10),
     allowNull: false,
+    field: 'request_method',
   })
   declare requestMethod: string;
 
   @Column({
     type: DataType.TEXT,
     allowNull: false,
+    field: 'request_path',
   })
   declare requestPath: string;
 
   @Column({
     type: DataType.JSONB,
     allowNull: true,
+    field: 'request_headers',
   })
   declare requestHeaders: Record<string, any>;
 
   @Column({
     type: DataType.JSONB,
     allowNull: true,
+    field: 'request_body',
   })
   declare requestBody: Record<string, any>;
 
   @Column({
     type: DataType.STRING,
     allowNull: true,
+    field: 'request_ip',
   })
   declare requestIp: string;
 
   @Column({
     type: DataType.INTEGER,
     allowNull: true,
+    field: 'response_status',
   })
   declare responseStatus: number;
 
   @Column({
     type: DataType.JSONB,
     allowNull: true,
+    field: 'response_body',
   })
   declare responseBody: Record<string, any>;
 
@@ -77,12 +85,14 @@ export class HypeStoreWebhookLog extends Model<HypeStoreWebhookLog> {
     type: DataType.BOOLEAN,
     defaultValue: false,
     allowNull: false,
+    field: 'is_valid',
   })
   declare isValid: boolean;
 
   @Column({
     type: DataType.TEXT,
     allowNull: true,
+    field: 'error_message',
   })
   declare errorMessage: string;
 
@@ -90,6 +100,7 @@ export class HypeStoreWebhookLog extends Model<HypeStoreWebhookLog> {
   @Column({
     type: DataType.INTEGER,
     allowNull: true,
+    field: 'processed_order_id',
   })
   declare processedOrderId: number;
 
@@ -97,6 +108,7 @@ export class HypeStoreWebhookLog extends Model<HypeStoreWebhookLog> {
     type: DataType.DATE,
     allowNull: false,
     defaultValue: DataType.NOW,
+    field: 'created_at',
   })
   declare createdAt: Date;
 
