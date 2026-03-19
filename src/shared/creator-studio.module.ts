57a2b9f63ad03736693ee13b7cc6775a0f151c6a
bhartimishra-dev <bharti.mishra@gobuymybills.com>
2026-03-19 11:00:27 +0530

    fix: issues fixed with import modules

--- src/shared/creator-studio.module.ts
+++ src/shared/creator-studio.module.ts
@@ -4,9 +4,22 @@ import { CreatorStudioController } from './creator-studio.controller';
 import { CreatorStudioService } from './creator-studio.service';
 import { Influencer } from '../auth/model/influencer.model';
 import { Brand } from '../brand/model/brand.model';
+import { Post } from '../post/models/post.model';
+import { PostView } from '../post/models/post-view.model';
+import { Like } from '../post/models/like.model';
+import { Share } from '../post/models/share.model';
 
 @Module({
-  imports: [SequelizeModule.forFeature([Influencer, Brand])],
+  imports: [
+    SequelizeModule.forFeature([
+      Influencer,
+      Brand,
+      Post,
+      PostView,
+      Like,
+      Share,
+    ]),
+  ],
   controllers: [CreatorStudioController],
   providers: [CreatorStudioService],
   exports: [CreatorStudioService],
