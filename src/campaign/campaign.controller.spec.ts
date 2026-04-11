ba118bc9f998ec4a44ebd4d387eb2522f4d6edb1
bhartimishra-dev <bharti.mishra@gobuymybills.com>
2026-04-11 11:11:45 +0530

    fix: test files issues fixed

--- src/campaign/campaign.controller.spec.ts
+++ src/campaign/campaign.controller.spec.ts
@@ -77,6 +77,7 @@ describe('CampaignController', () => {
         isOpenToAllGenders: true,
         deliverableFormat: ['instagram_post'],
         isOpenToAllAges: false,
+        numberOfInfluencers: 5,
       };
 
       const mockRequest: RequestWithUser = {
@@ -395,6 +396,7 @@ describe('CampaignController', () => {
         isOpenToAllGenders: true,
         deliverableFormat: ['instagram_post'],
         isOpenToAllAges: false,
+        numberOfInfluencers: 5,
       };
 
       const mockRequest: RequestWithUser = {
